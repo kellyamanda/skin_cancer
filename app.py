b187fc87379ef8bb9877d9a7ce55e559edc34753
Arun_Ramji_Shanmugam <40143199+ArunRamji@users.noreply.github.com>
2020-08-22 19:42:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if uploaded_file is not None:
     st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
-    st.write(type(uploaded_file))
+    #st.write(type(uploaded_file))
     label = machine_classification(uploaded_file,'model1.h5')
     my_bar = st.progress(0)
     for percent_complete in range(100):
