64c36c966023a4ba34ebbd983a96ad98c19e55ad
Arun_Ramji_Shanmugam <40143199+ArunRamji@users.noreply.github.com>
2020-10-03 22:44:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ if uploaded_file is not None:
     from keras.preprocessing import image 
     import numpy as np
      
+    st.write(type(uploaded_file))
     img = image.load_img(uploaded_file, target_size=(150, 150))
     img_tensor = image.img_to_array(img)
     img_tensor = np.expand_dims(img_tensor, axis=0)
