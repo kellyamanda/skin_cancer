916cdb63e83a4ab9f12d30ad21f58cbc26eaf34c
Arun_Ramji_Shanmugam <40143199+ArunRamji@users.noreply.github.com>
2020-10-03 22:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from Image_Classification import *
 import time
 #import PIL.Image as Image
 import base64
+from tensorflow.keras.models import load_model
+from keras import models
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
+
+
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -31,6 +37,56 @@ if uploaded_file is not None:
     st.write("")
     st.write("Classifying...")
     uploaded_file = uploaded_file.read()
+    st.write("")
+    st.write("Hi Doctor , Below are the sample images of how it looked like in my each layer of neural network...")
+    
+#     #Let's visualise all activation in the network
+#     from keras.models import load_model
+#     classifier = load_model('model1.h5')
+    
+    from keras.preprocessing import image 
+    import numpy as np
+     
+    img = image.load_img(uploaded_file, target_size=(150, 150))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+#   st.write(img_tensor.shape)
+    
+
+    layer_outputs = [layer.output for layer in classifier.layers[:8]] #Extract the output of top 4 layer
+    activation_model = Model(inputs=classifier.input, outputs=layer_outputs) 
+
+    activations = activation_model.predict(img_tensor) #Running model on predict mode
+
+    layer_names = []
+    for layer in classifier.layers[:4]:
+        layer_names.append(layer.name) #Name of the layer , so that we can plot
+    images_per_row = 16
+
+    for layer_name, layer_activation in zip(layer_names, activations): #Displays the feature map
+        n_features = layer_activation.shape[-1] #number of features in the feature map
+        size = layer_activation.shape[1] #The feature map has shape (1, size, size, n_features).
+        n_cols = n_features // images_per_row #tiles the actiavtion channel in the matrix
+        display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+        for col in range(n_cols):
+            for row in range(images_per_row):
+                channel_image = layer_activation[0,:,:,col*images_per_row+row]
+                channel_image -= channel_image.mean()
+                channel_image /= channel_image.std()
+                channel_image *= 64
+                channel_image += 128
+                channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+                display_grid[col * size : (col + 1) * size,
+                                  row * size : (row + 1) * size] = channel_image
+            scale =1. / size
+            plt.figure(figsize=(scale * display_grid.shape[1],scale * display_grid.shape[0])) 
+            plt.title(layer_name)
+            plt.grid(False)
+            plt.imshow(display_grid, aspect='auto', cmap='viridis')
+            st.pyplot()
+            
     #st.write(type(uploaded_file))
     #st.write(uploaded_file)
     label = machine_classification(uploaded_file,'model1.h5')
